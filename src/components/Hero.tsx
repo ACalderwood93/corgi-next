a6e3a7ff97ea7dd6bc3d2bfb8931fca1f0121f1a
Andrew Calderwood <andrew.calderwood@ovo.com>
2023-06-16 07:57:04 +0100

    WIP

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,18 @@
 "use client";
 import Link from "next/link";
 import Button from "react-bootstrap/Button";
+import { Roboto } from "next/font/google";
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: "700",
+});
 const hero = () => {
   return (
     <section>
-      <div className="bg-cover bg-no-repeat bg-[url('/hero.png')] text-center w-screen p-16">
+      <div
+        className={`bg-cover bg-no-repeat bg-[url('/hero.png')] text-center w-screen p-16 ${roboto.className}`}
+      >
         <h1>BOILER AND HOME EMERGENCY COVER</h1>
         <h1 className="text-white">FROM THE UK&apos;s TOP RATED PROVIDER</h1>
         <h3>Compare our boiler plans from £15 a month</h3>
--- src/components/HomeEmergencyCover.tsx
+++ src/components/HomeEmergencyCover.tsx
@@ -1,12 +1,13 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
+import CtaButton from "./CtaButton";
 
 const HomeEmergencyCover = () => {
   return (
     <div className="py-6 flex justify-center">
-      <div className="flex justify-center flex-row flex-wrap w-[70%]">
-        <div className="md:w-2/4 sm:w-full p-2">
+      <div className="flex justify-center flex-wrap w-[70%]">
+        <div className="w-full md:w-2/4 p-2">
           <span className="font-bold">Home Emergency cover</span>
           <h2 className="text-xl font-bold mb-2">
             Simple Plans to keep your mind at ease
@@ -29,12 +30,10 @@ const HomeEmergencyCover = () => {
           </ul>
 
           <Link href="/boiler-cover">
-            <button className="primary-colour p-2 rounded-md text-white font-bold">
-              Get boiler cover
-            </button>
+            <CtaButton>Get boiler cover</CtaButton>
           </Link>
         </div>
-        <div className="p-2 rounded-lg md:w-2/4 sm:w-full h-full">
+        <div className="rounded-lg w-full md:w-2/4 p-2 h-full">
           <Image
             className="aspect-auto"
             src="/home-emergency-cover.jpg"
