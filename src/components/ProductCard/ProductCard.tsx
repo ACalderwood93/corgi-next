967f1abeb282b8903e5a2b680e0ba64a75bfcd5b
Andrew Calderwood <andrew.calderwood@ovo.com>
2023-06-17 20:12:58 +0100

    styling product card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,35 +1,33 @@
-"use client";
 import { useState } from "react";
-import { IProduct } from "@/lib/Products/Interfaces";
+import { CoverSection, IProduct } from "@/lib/Products/Interfaces";
 import Card from "../Card";
 import CtaButton from "../CtaButton";
 import Link from "next/link";
 
 const ProductCard = (product: IProduct) => {
-  const [excess, setExcess] = useState<boolean>(false);
   return (
     <Card className="flex-grow p-6 mx-2 text-center mt-6 w-full md:w-full px-4 md:px-0 lg:w-32 md:mt-4">
       <h1>{product.name}</h1>
-      <div className="flex flex-col py-4 w-[80%] mx-auto">
+      <div className="flex flex-col py-4 w-[80%] mx-auto md:min-h-[400px]">
         <div>from</div>
         <h2>
           <span className="font-bold text-5xl text-primary">
-            £{excess ? product.price : product.priceWithoutExcess}
+            £{product.price}
           </span>
           <div>/ month</div>
         </h2>
-        <div className="text-left font-bold">Includes</div>
-        <ul className="text-left text-xs">
-          <li>Annual Boiler Service</li>
-          <li>Annual Boiler Service</li>
-          <li>Annual Boiler Service</li>
-          <li>Annual Boiler Service</li>
-          <li>Annual Boiler Service</li>
+        <div className="text-left font-bold mt-3">Includes</div>
+        <ul className="text-left text-sm">
+          {product.covered.map((section) => {
+            return <li key={section}>{CoverSection[section]}</li>;
+          })}
         </ul>
       </div>
-      <Link href={`/product/${product.name.toLowerCase()}`}>
-        <CtaButton className="p-4 w-[98%] md:w-[70%]">Select</CtaButton>
-      </Link>
+      <div className="w-full">
+        <Link className="" href={`/product/${product.name.toLowerCase()}`}>
+          <CtaButton className="p-4 w-[98%] md:w-[70%]">Select</CtaButton>
+        </Link>
+      </div>
     </Card>
   );
 };
---
+++ src/lib/CoverSections/constants.ts
@@ -0,0 +1,21 @@
+import { CoverSection, ICoverSectionParent } from "../Products/Interfaces";
+
+export const CoverSections: ICoverSectionParent[] = [
+  {
+    name: CoverSection.AnnualBoilerService,
+    sections: [
+      "The Annual boiler service does not form part of the insurance contract",
+      "A visual check of the boiler",
+      "The boiler being fired safely to identify any working faults",
+      "A flue gas analysis efficiency test",
+      "Opening up the boiler and inspecting it, if necessary",
+      "Checking the flue and ventilation are in accordance with Gas Safety (Installation and Use) Regulations",
+      "Checking and adjusting the system pressure",
+      "Cleaning the condensate trap",
+      "Visually checking and bleeding radiators, if necessary",
+      "A visual check of the hot water cylinder",
+      "Confirmation in writing that the service has been carried out",
+      "Cleaning of the boiler’s filter (if you ask us to)",
+    ],
+  },
+];
