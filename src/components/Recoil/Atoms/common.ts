eae7866e21a5a84c24ef156204b5b166caa3134b
Andrew Calderwood <andrew.calderwood@ovo.com>
2023-06-18 16:24:47 +0100

    updates

--- src/components/Recoil/Atoms/common.ts
+++ src/components/Recoil/Atoms/common.ts
@@ -1,6 +1,3 @@
 import { recoilPersist } from "recoil-persist";
 
-export const { persistAtom } = recoilPersist({
-  key: "recoil-persist", // this key is using to store data in local storage
-  storage: typeof window !== "undefined" ? localStorage : undefined, // configurate which stroage will be used to store the data
-});
+export const { persistAtom } = recoilPersist();
--- src/components/Recoil/Checkout/Atoms/Checkout.ts
+++ src/components/Recoil/Checkout/Atoms/Checkout.ts
@@ -2,14 +2,15 @@ import { Excess, ICheckout } from "@/lib/Checkout/interfaces";
 import { atom } from "recoil";
 import { persistAtom } from "../../Atoms/common";
 
+export const defaultValue: ICheckout = {
+  excess: Excess.Zero,
+  personalDetails: undefined,
+  address: undefined,
+  bankDetails: undefined,
+  product: undefined,
+};
 export const checkoutState = atom<ICheckout>({
   key: "checkoutState",
-  default: {
-    excess: Excess.Zero,
-    personalDetails: undefined,
-    address: undefined,
-    bankDetails: undefined,
-    product: undefined,
-  },
+  default: defaultValue,
   effects_UNSTABLE: [persistAtom],
 });
